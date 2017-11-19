bc4cce2b0a539619dad2900fb07a42ac25768b40
Damian Dalle Nogare <damiandn@gmail.com>
2017-11-19 22:55:50 +0000

    add app file

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+
+
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));   
+
+
+//ROUTES
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.get("/bio", function(req, res){
+    res.render("bio");
+});
+
+
+app.get("/publications", function(req, res){
+    res.render("publications");
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+   console.log("Server Started!");
+});
+
+
+
+

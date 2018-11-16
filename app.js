7a3a4e86de54490cc021401e73282837a2bfebfa
Damian Dalle Nogare <damiandn@gmail.com>
2018-11-16 02:42:09 +0000

    add start script for deployment

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require("express");
-
-
 var app = express();
 
 app.set("view engine", "ejs");
@@ -17,11 +15,35 @@ app.get("/bio", function(req, res){
     res.render("bio");
 });
 
+app.get("/cv", function(req, res){
+    res.render("cv");
+});
+
+
+app.get("/interests", function(req, res){
+    res.render("interests");
+});
+
 
 app.get("/publications", function(req, res){
     res.render("publications");
 });
 
+
+app.get("/resources/sequences", function(req, res) {
+   res.render("resources/sequences");
+    
+});
+
+app.get("/resources/tools", function(req, res) {
+   res.render("resources/tools");
+    
+});
+
+app.get("/microscopy", function(req, res) {
+   res.render("microscopy");
+    
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log("Server Started!");
